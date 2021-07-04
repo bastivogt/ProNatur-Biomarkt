b2aecc9098a7d9118d1fb8d765f35db20f30e38b
Sebastian Vogt <betont.online@googlemail.com>
2021-07-04 22:34:34 +0200

    Adding loadingprogressbar update from 1 to 2

---
+++ LoadingScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProNatur_Biomarkt_GmbH
+{
+    public partial class loadingScreen : Form
+    {
+
+        int loadingbarValue;
+
+        public loadingScreen()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void loadingScreen_Load(object sender, EventArgs e)
+        {
+            
+            loadingbarTimer.Start();
+        }
+
+        private void loadingbarTimer_Tick(object sender, EventArgs e)
+        {
+            loadingbarValue += 2;
+            loadingProgressbar.Value = loadingbarValue;
+            lblLoadingProgress.Text = $"{loadingbarValue}%";
+            Timer s = sender as Timer;
+            if(loadingbarValue >= loadingProgressbar.Maximum)
+            {
+                s.Stop();
+            }
+        }
+    }
+}
